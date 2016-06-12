7923ae5538c7c00ff14dd2c087fb9b1935911c14
F <fwrizalman@gmail.com>
2016-06-12 18:28:07 +0800

    Create movie trailer project

---
+++ ud036/movie_trailer/entertainment_center.py
@@ -0,0 +1,7 @@
+import media
+
+silicon_valley = media.Movie('Silicon Valley',
+                             'The series focuses on six young men who found a startup company in Silicon Valley',
+                             'https://en.wikipedia.org/wiki/File:Silicon_valley_title.png',
+                             'https://www.youtube.com/watch?v=AOYHABzlpyQ')
+print(silicon_valley.storyline)
